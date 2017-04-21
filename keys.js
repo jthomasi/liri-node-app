434d02d1806080fc2c1f0e05ef3717e682c4afcf
jthomasi <jayayetea@gmail.com>
2017-04-20 20:18:55 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'ewFekyhiUE4wcMzMCpcI1D2yS',
-  consumer_secret: 'n2yeYSSVpsWxzyKAuQEYIPDXzuhnHch56vuxp4YWJM3Fbtlxqu',
-  access_token_key: '846516721280303104-5IoeUOB35FTvHIgvTdsCMI2E7dvwtAH',
-  access_token_secret: 'bBdz3fYAE3bOoh82PAiyNk0ktRgui0jrPdThvWIhRZYrV'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 };
 
